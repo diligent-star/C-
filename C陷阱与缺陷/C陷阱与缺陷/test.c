cfd06f53f708cd933addc7705bd58f89473ae1cf
diligent-star <69176650+diligent-star@users.noreply.github.com>
2021-01-12 22:54:22 +0800

    1，2章阅读

---
+++ C陷阱与缺陷/C陷阱与缺陷/test.c
@@ -0,0 +1,47 @@
+#include "test.h"
+
+int main()
+{
+	//int a = 0;
+	//a = 77;
+	//printf("%d ",a);
+	//a = 077;
+	//printf("%d ",a);
+
+	//printf("Hello World\n");
+	//char hello[] = {'H','e','l','l','o',' ','W','o','r','l','d','\n',0};
+	//printf(hello);
+
+	//printf('\n');
+
+	//三目操作符 优先于 赋值
+	//int a = 0;
+	//int b = 1;
+	//int c = 0;
+	//c = a > b ? 8 : 9;
+	//printf("%d ",c);
+
+	// /* /**/ "*/ " /*" /**/
+	;
+	// printf("%d",/*/*/0 */**/1);
+
+	/*	int days[] = {
+	1,2,
+	3,4,
+	5,6,
+	};*/
+	/*
+
+	;
+
+	/*
+
+	;
+
+	*/
+
+	/*
+	;
+	*/
+	return 0;
+}
